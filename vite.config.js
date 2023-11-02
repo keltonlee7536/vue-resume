051381c34b0e664d7bad35db7ac812bb88abc71f
keltonlee7536 <kelton.lee7536@stu.mtec.edu>
2023-11-02 11:16:42 -0600

    11.2 adding dist

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  // publicDir: '/vue-resume',//at some point there is an extra 'vue-resume'.
+  publicDir: '/vue-resume',
   base: '/vue-resume',
   plugins: [
     vue(),
