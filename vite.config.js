e52ef04d4048c9a6e3908a9e6c1c43d08b525a83
keltonlee7536 <kelton.lee7536@stu.mtec.edu>
2023-11-02 10:51:55 -0600

    file-configs-v2

--- vite.config.js
+++ vite.config.js
@@ -5,13 +5,13 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  publicDir: '/vue-resume',
+  // publicDir: '/vue-resume',//at some point there is an extra 'vue-resume'.
   base: '/vue-resume',
   plugins: [
     vue(),
   ],
   resolve: {
-    alias: {
+    alias: {// i think './vue-resume' should be empty.
       '@': fileURLToPath(new URL('./vue-resume', import.meta.url))//switched 11/2 from './src' to './vue-resume'
     }
   }
