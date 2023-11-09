bc275ddf6dcca8a0231868cae4f38f0a0e0f8b5d
keltonlee7536 <kelton.lee7536@stu.mtec.edu>
2023-11-09 12:03:17 -0700

    projects page image test

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  publicDir: '/vue-resume',
+  // publicDir: '/vue-resume',
   base: '/vue-resume',
   plugins: [
     vue(),
