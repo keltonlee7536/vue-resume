ae3e4e9ab0394a329830915b257e5c2e476101c5
keltonlee7536 <kelton.lee7536@stu.mtec.edu>
2023-11-09 11:50:46 -0700

    image debugging

--- src/components/skills.vue
+++ src/components/skills.vue
@@ -13,5 +13,6 @@ export default {
 <template>
     <div class="icon-grid-parent">
         <img  v-for="images in skillsJson" class="icon icon-margin" :src="images.iconPath">
+        <img class="icon icon-margin"  src="../../public/no-profile-image.webp">
     </div>
 </template>
