e7e0a3c35c6dc783aaed54ee6d66b92597dd9d84
keltonlee7536 <kelton.lee7536@stu.mtec.edu>
2023-11-16 14:44:14 -0700

    footer mobile spacing, downloads/links

--- src/components/footer.vue
+++ src/components/footer.vue
@@ -1,11 +1,11 @@
 
 <template>
     <div class="dark-grey-card">
-        <p class="indent-left"><strong>Contact Me</strong></p>
-        <div class="navbar-structure indent-left footer- no-vertical-margin">
-            <a href = "./kelton-lee-resume.pdf" download = "kelton-lee-resume">download a .pdf</a>
-            <a class="indent-left" href="mailto:kslquartz@gmail.com">kslquartz@gmail.com</a>
-            <a class="indent-left" href="https://www.linkedin.com/in/kelton-lee-78b239249/">Linkedin</a>
+        <p class="text-center"><strong>Contact Me</strong></p>
+        <div class="navbar-structure footer- no-vertical-margin footer-flex">
+            <a href = "./kelton-lee-resume.pdf" download = "kelton-lee-resume" target="blank">download a .pdf</a>
+            <a class="indent-left" href="mailto:kslquartz@gmail.com" target="blank">kslquartz@gmail.com</a>
+            <a class="indent-left" href="https://www.linkedin.com/in/kelton-lee-78b239249/" target="blank">Linkedin</a>
         </div>
     </div>
 </template>
