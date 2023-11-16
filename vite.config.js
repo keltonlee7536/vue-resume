8066eb7e2b98f83b4329ff301d9ee130cb96ebc5
keltonlee7536 <kelton.lee7536@stu.mtec.edu>
2023-11-16 12:29:30 -0700

    vite image file paths edits I

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   // publicDir: 'assets',
-  base: '/vue-resume',
+  base: '/vue-resume/',
   plugins: [
     vue(),
   ],
