4ec4356f4463f8964e790c4f0934dd3724197f0d
keltonlee7536 <kelton.lee7536@stu.mtec.edu>
2024-09-04 10:50:24 -0600

    general and bio updates

--- src/components/bio.vue
+++ src/components/bio.vue
@@ -9,14 +9,14 @@
 
     <div class="bio-wrapper">
         <!-- <img class="bio-image" src="../assets/images/no-profile-image.webp"> -->
-        <img class="bio-image" src="/no-profile-image.webp">
-        <!-- <img src="/no-profile-image.webp"> -->
-
+        <!-- <img class="bio-image" src="/no-profile-image.webp"> -->
+        <!-- <img class="bio-image" src="/kel-and-pablo.jpeg"> -->
+        <img class="bio-image" src="/kel-profile.jpeg">
 
         <div class="indent-left">
             <h1>Kelton Lee</h1>
             <h3 class="title">Web Developer</h3>
-            <p>"Experienced web developer with a passion for creating responsive, user-friendly websites. Proficient in HTML, CSS/Scss JavaScript, and a range of JavaScript frameworks. Skilled in optimizing website performance and ensuring cross-browser compatibility. Strong problem-solving abilities and a commitment to staying updated with the latest industry trends. Committed to delivering high-quality web solutions."</p>
+            <p>"Hi! I'm Kelton. I'm a web developer with a passion for building, exploring, and (inevitably)fixing. I was introduced to programming python in middle school and introduced to Web Development ~2020. since then I began making projects and watching youtube lectures, took a certificate course at Mtec, had an internship at MasterControl, and am now pursuing a college degree at Utah Valley University."</p>
         </div>
     </div>
 </template>
